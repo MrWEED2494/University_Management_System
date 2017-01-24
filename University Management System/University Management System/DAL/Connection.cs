1cc4203206a823cbc96f263838629e4d8e08f181
Md. Akmal Hossain <akmal2494@gmail.com>
2017-01-25 01:24:51 +0600

    last upload

---
+++ University Management System/University Management System/DAL/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace University_Management_System.DAL
+{
+    public class Connection
+    {
+        public string GetConnection()
+        {
+            string connectionString = @"Data Source=ShahinAhmed;Initial Catalog=UniversityManagement; Uid=sa; Password=132188";
+
+            return connectionString;
+
+        }
+    }
+}
